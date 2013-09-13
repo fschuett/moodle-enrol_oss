a04c5c51ac955fa8aa1a7c0e8a41558edc32a627
fschuett <fschuett@gymnasium-himmelsthuer.de>
2013-09-13 17:36:31 +0200

    changed version number

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013051700;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2013091300;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2010112400;        // Requires this Moodle version.
 $plugin->component = 'enrol_openlml';   // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 60*60;             // Run cron every hour, because it is time consuming.
 $plugin->maturity  = MATURITY_BETA;     // Beta, nees testing.
-$plugin->release   = '0.6 (Build: 2013051700)';
+$plugin->release   = '0.7 (Build: 2013091300)';
 $plugin->dependencies = array('auth_ldap'=>ANY_VERSION, 'enrol_cohort'=>ANY_VERSION);

abf973704296b57b1017098e0cd630de08ed261f
Frank Schütte <fschuett@gymhim.de>
2019-11-02 07:46:42 +0100

    remove debug function

--- lib.php
+++ lib.php
@@ -13,9 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-# debug function
-function kill($data){ var_dump($data); exit; }
-@ini_set('display_errors','1');
+
 /**
  * OSS enrolment plugin implementation.
  *

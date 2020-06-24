e1cba15c6715b6f8976577dd2992f870f23487fd
fschuett <fschuett@gymnasium-himmelsthuer.de>
2020-06-24 15:22:43 +0200

    version 2020062401

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020032701;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020062401;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015051100;        //  Requires Moodle version 2.9
 $plugin->component = 'enrol_oss';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_BETA;     // Beta, nees testing.
-$plugin->release   = '2.0 (Build: 2020032701)';
+$plugin->release   = '2.0 (Build: 2020062401)';
 $plugin->dependencies = array('auth_ldap'=>ANY_VERSION, 'enrol_cohort'=>ANY_VERSION);
